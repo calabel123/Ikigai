05d4872b0d1269ca505fba3a19b155aff7f7d52d
ShotaroBaba <bshotaro@gmail.com>
2019-09-14 16:07:04 +1000

    First Ikigai Nao commit.

---
+++ IKIGAI Nao/IKIGAI/IKIGAI.pml
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="IKIGAI" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="" src=".metadata" />
+        <File name="Ikigai_1_resized" src="html/Ikigai_1_resized.jpg" />
+        <File name="Ikigai_2_resized" src="html/Ikigai_2_resized.png" />
+        <File name="pepper_1" src="html/pepper_1.jpg" />
+        <File name="sunny_resized" src="html/sunny_resized.jpg" />
+        <File name="cloudy_resized" src="html/cloudy_resized.jpg" />
+        <File name="Dancing_resized" src="html/Dancing_resized.gif" />
+        <File name="fall_down_1_resized" src="html/fall_down_1_resized.jpg" />
+        <File name="game_2_resized" src="html/game_2_resized.jpg" />
+        <File name="Ikigai_0_resized" src="html/Ikigai_0_resized.png" />
+        <File name="Ikigai_2_resized" src="Ikigai_2_resized.png" />
+        <File name="Ikigai_3_resized" src="html/Ikigai_3_resized.png" />
+        <File name="Ikigai_4_resized" src="html/Ikigai_4_resized.png" />
+        <File name="karaoke_resized" src="html/karaoke_resized.jpg" />
+        <File name="monkey_fall_from_the_tree_resized" src="html/monkey_fall_from_the_tree_resized.jpg" />
+        <File name="notseeingflower_resized" src="html/notseeingflower_resized.png" />
+        <File name="SurfingMistake1_resized" src="html/SurfingMistake1_resized.jpg" />
+        <File name="sad_resized" src="html/sad_resized.png" />
+        <File name="translation_en_US" src="translations/translation_en_US.ts" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+</Package>
---
+++ IKIGAI Nao/IKIGAI/translations/translation_en_US.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source> Hello, I am Pepper \\pau=4000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </source>
+            <comment>Text</comment>
+            <translation type="obsolete"> Hello, I am Pepper \\pau=4000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source> Hello, I am Pepper. \\pau = 1000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </source>
+            <comment>Text</comment>
+            <translation type="obsolete"> Hello, I am Pepper. \\pau = 1000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </translation>
+        </message>
+        <message>
+            <source> Hello, I am Pepper \\pau=1000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </source>
+            <comment>Text</comment>
+            <translation type="obsolete"> Hello, I am Pepper \\pau=1000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </translation>
+        </message>
+        <message>
+            <source> Hello, I am Pepper \\pau=4000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </source>
+            <comment>Text</comment>
+            <translation type="obsolete"> Hello, I am Pepper \\pau=4000\\ How are you today? Bright and happy like a sunny day, or there are a few clouds around, and feeling downed under the weather. </translation>
+        </message>
+    </context>
+</TS>
